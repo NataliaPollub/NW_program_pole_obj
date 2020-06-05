dc5c842d25653246a7783beb7138ef4282aa748b
NataliaPollub <natalia.widomska@pollub.edu.pl>
2020-06-05 15:39:24 +0200

    Add objetosc.c file

---
+++ source/objetosc.c
@@ -0,0 +1,7 @@
+#include "objetosc.h"
+
+void objetosc(float b1, float b2, float h)
+{
+    printf("\nProstopadloscian o wymiarach: %.2f x %.2f x %.2f\n", b1, b2, h);
+    printf("Objetosc prostopadloscianu: %.2f\n", b1*b2*h);
+}
